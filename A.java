6b8c6ae9297783cb02f6d6eff997f0c427997f21
Adit Vijay <Administrator@DIN86000537.corp.capgemini.com>
2017-09-15 10:13:26 +0530

    tester commit

--- A.java
+++ A.java
@@ -7,6 +7,7 @@ system.output.println("Welcome to GITHUB");
 system.output.println("Welcome to Master");
 
 system.output.println("Welcome to DevOne");
+system.output.println("Welcome to Tester");
 
 
 
